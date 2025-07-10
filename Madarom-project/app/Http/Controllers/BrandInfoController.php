fb21b2381d19d0265cce103218812a7910391aed
rajaonera <hugueszeus@gmail.com>
2025-07-10 16:25:22 +0300

    models -0

---
+++ Madarom-project/app/Http/Controllers/BrandInfoController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BrandInfo;
+use Illuminate\Http\Request;
+
+class BrandInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BrandInfo $brandInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BrandInfo $brandInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BrandInfo $brandInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BrandInfo $brandInfo)
+    {
+        //
+    }
+}
