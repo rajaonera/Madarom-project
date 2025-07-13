9406ad58721e72f84f602dd9a629f07abb98d96b
madarom <madarom.compte10@gmail.com>
2025-07-13 20:47:31 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/setup-storage-link', function () {
+    Artisan::call('storage:link');
+    return '✅ Lien symbolique storage/public créé avec succès.';
 });
